e9217c074ff1d6d6df3f4e2dbfdc86204de53718
Ron Dadon <rondadon1@gmail.com>
2015-05-12 18:14:30 +0300

    Update

--- Public/js/application.js
+++ Public/js/application.js
@@ -1,8 +1,19 @@
 var autoLogout;
+
+function resetAutoLogout()
+{
+    clearTimeout(autoLogout);
+    autoLogout = setTimeout(function() { $('#auto-logout-modal').modal('show'); }, 5000);
+}
+
 $(document).on('ready', function() {
     autoLogout = setTimeout(function() { $('#auto-logout-modal').modal('show'); }, 5000);
 });
+
 $(document).on('mousemove', function(event){
-    clearTimeout(autoLogout);
-    autoLogout = setTimeout(function() { $('#auto-logout-modal').modal('show'); }, 5000);
+    resetAutoLogout();
+});
+
+$(document).on('keypress', function(event){
+    resetAutoLogout();
 });
