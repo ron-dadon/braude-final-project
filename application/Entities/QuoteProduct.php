6231ec06393c48e8f6c6210244a0b7f76d896405
guy-franco <guyfranco1@walla.com>
2015-06-09 15:34:47 +0300

    new validation

--- application/Entities/QuoteProduct.php
+++ application/Entities/QuoteProduct.php
@@ -26,7 +26,21 @@ class QuoteProduct extends Entity
     public function isValid()
     {
         $valid = parent::isValid();
-
+        if (!$this->isFloat($this->Price, 0))
+        {
+            $valid = false;
+            $this->setError('Price', "Price must be in a valid format");
+        }
+        if (!$this->isInteger($this->quantity, 1))
+        {
+            $valid = false;
+            $this->setError('quantity', "uantity is invalid");
+        }
+        if (!$this->isString($this->comment, 0,2000 ))
+        {
+            $valid = false;
+            $this->setError('comment', "Comment length can't exceed 2000 characters");
+        }
     }
 
 } 
